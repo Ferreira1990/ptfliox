495642674d20e53d5d3926a546c9567d68dab44e
Cristian Ferreira <ferreira.8390mail.com>
2023-11-18 23:22:38 -0300

    imgfix

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import '../Home/Home.css'
 
+import Meicon from '../../assets/me.png'
+import lnk from '../../assets/linkedin.png'
+import tlg from '../../assets/Telegram.png'
+
 const Home = () => {
   return (
     <>
     <div className='Container'>
       <div className='card'>
         <div className='Profilepic'>
-          <img className='Me' src="src\assets\me.png" alt="" />          
+          <img className='Me' src={Meicon} alt="" />          
         </div>
 
         <div className='MyName'>
@@ -20,8 +24,12 @@ const Home = () => {
         </div>
 
         <div className='SocialMedia'>
-          <a href="https://www.linkedin.com/in/cristi%C3%A1n-ferreira-664241227/" target="_blank" rel="noopener noreferrer"><img src="src\assets\linkedin.png" alt="" /></a>
-          <a href="https://t.me/Cferreirap" target="_blank" rel="noopener noreferrer"><img src="src\assets\Telegram.png" alt="" /></a>     
+          <a href="https://www.linkedin.com/in/cristi%C3%A1n-ferreira-664241227/" target="_blank" rel="noopener noreferrer">
+            <img src={lnk} alt="" />
+            </a>
+          <a href="https://t.me/Cferreirap" target="_blank" rel="noopener noreferrer">
+            <img src={tlg} alt="" />
+            </a>     
         </div>
 
 
