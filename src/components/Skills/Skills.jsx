2879bbda207da573052026b04521e4a9b66671a6
Cristian Ferreira <ferreira.8390mail.com>
2023-11-18 23:17:55 -0300

    skills

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,20 +1,30 @@
 import React from 'react'
 import '../Skills/Skills.css'
 
+import htmlimg from '../../assets/htmlicon.png'
+import css from '../../assets/cssicon.png'
+import Git from '../../assets/giticon.png'
+import Boots from '../../assets/Bootstrapicon.png'
+import Jsicon from '../../assets/Jsicon.png'
+import Reacticon from '../../assets/Reacticon.png'
+import TWind from '../../assets/Tailwindicon.png'
+import Muicon from '../../assets/Muiicon.png'
+
+
 const Skills = () => {
     return (
         <>
             <div id='Skillz' className='Skillsbox'>
                 <h2>Mis Habilidades (My Skills).</h2>
                 <div className='Skills'>
-                    <img src="src\assets\htmlicon.png" alt="HTML Icon" />
-                    <img src="src\assets\cssicon.png" alt="CSS Icon" />
-                    <img className='Gitcstm' src="src\assets\giticon.png" alt="Github Icon" />
-                    <img src="src\assets\Bootstrapicon.png" alt="Bootstrap Icon" />
-                    <img src="src\assets\Jsicon.png" alt="Jscript icon" />
-                    <img src="src\assets\Reacticon.png" alt="React Icon" />
-                    <img src="src\assets\Tailwindicon.png" alt="Tailwind icon" />
-                    <img src="src\assets\Muiicon.png" alt="Material UI icon" />
+                    <img src={htmlimg} alt="HTML Icon" />
+                    <img src={css} alt="CSS Icon" />
+                    <img className='Gitcstm' src={Git} alt="Github Icon" />
+                    <img src={Boots} alt="Bootstrap Icon" />
+                    <img src={Jsicon} alt="Jscript icon" />
+                    <img src={Reacticon} alt="React Icon" />
+                    <img src={TWind} alt="Tailwind icon" />
+                    <img src={Muicon} alt="Material UI icon" />
                 </div>
             </div>
         </>
