817f2cf0c64778561f2ca3a9dcabe11006805da4
Cristian Ferreira <ferreira.8390mail.com>
2023-12-21 22:41:27 -0300

    tok

--- src/components/Works/Works.jsx
+++ src/components/Works/Works.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import '../Works/Works.css'
 import Camila from '../../assets/Camila.jpg'
 import Pizza from '../../assets/Pizza.jpg'
-import Form from '../../assets/Logform.jpg'
+import Tok from '../../assets/Tok.jpg'
 import lionel from '../../assets/LUA.jpg'
 
 const Works = () => {
@@ -14,21 +14,8 @@ const Works = () => {
 
         <div className='Glasscontainer'>
 
-            
-<div className='workscontainerx'>
-    <a href="https://logincolor.vercel.app/" target="_blank" rel="noopener noreferrer">
-    <img className='camilawork' src={Form} alt="Form Work" />
-    </a>
-    
-    <div></div>
-    <div className='orderskills'><button className='experience'>HTML</button> <button className='experience'>CSS</button>
-    <button className='experience'>Pshop</button>
-    <button className='experience'>Javascript</button> <button className='experience'>Tailwind</button>
-    </div>
-    <button className='experience'>React + VITE</button>
-</div>
 
-<div className='workscontainerx'>
+        <div className='workscontainerx'>
     <a href="https://agencialua.cl/" target="_blank" rel="noopener noreferrer">
     <img className='camilawork' src={lionel} alt="Form Work" />
     </a>
@@ -40,6 +27,21 @@ const Works = () => {
     </div>
     <button className='experience'>React + VITE</button>
 </div>
+
+            
+<div className='workscontainerx'>
+    <a href="https://tok-mauve.vercel.app/" target="_blank" rel="noopener noreferrer">
+    <img className='camilawork' src={Tok} alt="Form Work" />
+    </a>
+    
+    <div></div>
+    <div className='orderskills'><button className='experience'>HTML</button> <button className='experience'>CSS</button>
+    <button className='experience'>Pshop</button>
+    <button className='experience'>Javascript</button> <button className='experience'>Tailwind</button>
+    </div>
+    <button className='experience'>React + VITE</button>
+</div>
+
 
             <div className='workscontainerx'>
                 <a href="https://ferreira1990.github.io/camilacuevas/" target="_blank" rel="noopener noreferrer">
